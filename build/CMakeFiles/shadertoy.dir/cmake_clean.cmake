d6c588acebfce90aad56e8164294eaa79882540c
matt <myounger009@gmail.com>
2022-10-23 15:44:21 +0100

    Initial Commit

---
+++ build/CMakeFiles/shadertoy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shadertoy.dir/source/main.cxx.o"
+  "CMakeFiles/shadertoy.dir/source/main.cxx.o.d"
+  "shadertoy"
+  "shadertoy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shadertoy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source/main.cxx
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string>
+
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+int main(int, char**) {
+  glfwInit();
+  printf("Hello, World!\n");
+  return 0;
+}
